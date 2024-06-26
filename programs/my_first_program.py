4f76ffe6688bd506388aebd55880a8158d9ccdde
rohitkumr <rohit23021997@gmail.com>
2024-06-26 18:53:40 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,23 @@
 from nada_dsl import *
 
-
 def nada_main():
     party1 = Party(name="Party1")
+    party2 = Party(name="Party2")
+    
+    # Inputs from different parties
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    my_int2 = SecretInteger(Input(name="my_int2", party=party2))
+    
+    # Perform an addition operation between two secret integers from different parties
+    new_int = my_int1 + my_int2
+    
+    # Return outputs for both parties
+    output_for_party1 = Output(new_int, "result_for_party1", party1)
+    output_for_party2 = Output(new_int, "result_for_party2", party2)
+    
+    return [output_for_party1, output_for_party2]
 
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
+# This program demonstrates inter-party computation where two different parties
+# input their secret integers and get the result of their sum without revealing
+# their inputs to each other.
 
-    return [Output(my_int1, "my_output", party1)]
