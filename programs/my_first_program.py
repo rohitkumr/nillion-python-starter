2ae75583be3894496a2fbd3e1ff360f724612aa0
rohitkumr <rohit23021997@gmail.com>
2024-07-05 19:55:30 +0530

    Update my_first_program.py

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,23 +1,46 @@
 from nada_dsl import *
-
+"""
+PROGRAM 2
+nr of stores: m = 3
+nr of products: n = 2
+"""
+from nada_dsl import *
+#store program
 def nada_main():
     party1 = Party(name="Party1")
-    party2 = Party(name="Party2")
-    
-    # Inputs from different parties
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party2))
-    
-    # Perform an addition operation between two secret integers from different parties
-    new_int = my_int1 + my_int2
-    
-    # Return outputs for both parties
-    output_for_party1 = Output(new_int, "result_for_party1", party1)
-    output_for_party2 = Output(new_int, "result_for_party2", party2)
-    
-    return [output_for_party1, output_for_party2]
+    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-# This program demonstrates inter-party computation where two different parties
-# input their secret integers and get the result of their sum without revealing
-# their inputs to each other.
+    # Performing addition operation on the two input integers
+    result = my_int1 + my_int2
+    # 1. Parties initialization
+    store0 = Party(name="Store0")
+    store1 = Party(name="Store1")
+    store2 = Party(name="Store2")
+    outparty = Party(name="OutParty") 
+
+    # 2. Inputs initialization
+    ## Sales from store 0
+    s0_p0 = SecretUnsignedInteger(Input(name="s0_p0", party=store0))
+    s0_p1 = SecretUnsignedInteger(Input(name="s0_p1", party=store0))
+    ## Sales from store 1
+    s1_p0 = SecretUnsignedInteger(Input(name="s1_p0", party=store1))
+    s1_p1 = SecretUnsignedInteger(Input(name="s1_p1", party=store1))
+    ## Sales from store 2
+    s2_p0 = SecretUnsignedInteger(Input(name="s2_p0", party=store2))
+    s2_p1 = SecretUnsignedInteger(Input(name="s2_p1", party=store2))
+
+    # 3. Computation
+    ## Add sales for product 0
+    result_p0 = s0_p0 + s1_p0 + s2_p0
+    ## Add sales for product 1
+    result_p1 = s0_p1 + s1_p1 + s2_p1
+
+    # 4. Output
+    result_p0 = Output(result_p0, "final_sales_count_p0", outparty)
+    result_p1 = Output(result_p1, "final_sales_count_p1", outparty)
+
+    # Output the result of the addition operation
+    return [Output(result, "result_output", party1)]
+    return [result_p0, result_p1]
 
